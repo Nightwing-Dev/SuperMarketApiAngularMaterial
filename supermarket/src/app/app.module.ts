5a606e75aade6b4e4dc61141d360b67dc4b5ca64
Nightwing-Dev <diego77andre77@gmail.com>
2022-06-12 18:53:57 -0500

    Creacion de las validaciones del formulario y api

--- supermarket/src/app/app.module.ts
+++ supermarket/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -33,7 +35,11 @@ import { MatRadioModule } from '@angular/material/radio';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatRadioModule
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- supermarket/src/app/dialog/dialog.component.ts
+++ supermarket/src/app/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { FormGroup, FormBuilder,Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-dialog',
@@ -8,21 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
- articulos = ["Articulo nuevo", "Articulo Usado", "Articulo Reacondicionado"]
- 
-  //productForm!: FormGroup;
-  //constructor(private formBuilders: FormBuilder) { }
+  articulos = ["Articulo nuevo", "Articulo Usado", "Articulo Reacondicionado"]
+
+  productForm!: FormGroup;
+  constructor(private formBuilders: FormBuilder, private apiSvc: ApiService) { }
 
   ngOnInit(): void {
-    /*this.productForm = this.formBuilders.group({
+    this.productForm = this.formBuilders.group({
       productName: ['', Validators.required],
       category: ['', Validators.required],
-      estado :['',Validators.required],
-      precio:['', Validators.required],
-      comentarios:['', Validators.required],
-      date: ['',Validators.required]
+      stade: ['', Validators.required],
+      price: ['', Validators.required],
+      comments: ['', Validators.required],
+      date: ['', Validators.required]
     })
   }
+  addProduct() {
+    if (this.productForm.valid) {
+      this.apiSvc.postProduct(this.productForm.value).subscribe({
+        next: (res) => {
+          alert("producto agregado correctamente mi perro")
+        },
+        error: () => {
+          alert("tu aplicacion esta del culo cambiala")
+        }
+      })
+    }
+  }
+}
 
-}*/
-  }}
+
